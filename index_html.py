1f0f9440a639e11e58047a860b2240d6a38ae71e
Son Tran <sontran@berkeley.edu>
2020-05-10 17:06:03 -0700

    Seperate the two types of the website: paper release and for the final project

--- index_html.py
+++ index_html.py
@@ -1,25 +1,43 @@
 #!/usr/bin/env python
 
-def createIndex():
-    filenames = ['./html/head.html',
-                 './html/title.html',
-                 './html/authors.html',
-                 './html/school.html',
-                 './html/image.html',
-                 './html/abstract.html',
-                 './html/paper.html',
-                 './html/code.html',
-                 './html/dataset.html',
-                 # './html/demo.html',
-                 './html/results.html',
-                 './html/acknowledgements.html']
+def generate_index(filenames):
     with open('./index.html', 'w') as outfile:
         for fname in filenames:
             with open(fname) as infile:
                 outfile.write(infile.read())
 
 def main():
-    createIndex()
+    # paper_filenames = [
+    #         './html/paper/head.html',
+    #         './html/paper/title.html',
+    #         './html/paper/authors.html',
+    #         './html/paper/school.html',
+    #         './html/paper/image.html',
+    #         './html/paper/abstract.html',
+    #         './html/paper/paper.html',
+    #         './html/paper/code.html',
+    #         './html/paper/dataset.html',
+    #         './html/paper/demo.html',
+    #         './html/paper/results.html',
+    #         './html/paper/acknowledgements.html'
+    #         ]
+    proj_filenames = [
+            './html/proj/head.html',
+            './html/proj/title.html',
+            './html/proj/authors.html',
+            './html/proj/school.html',
+            './html/proj/image.html',
+            './html/proj/abstract.html',
+            './html/proj/paper.html',
+            './html/proj/code.html',
+            './html/proj/dataset.html',
+            './html/proj/results.html',
+            './html/proj/conclusion.html',
+            './html/proj/bios.html',
+            './html/proj/acknowledgements.html'
+            ]
+    # generate_index(paper_filenames)
+    generate_index(proj_filenames)
 
 if __name__ == '__main__':
     main()
