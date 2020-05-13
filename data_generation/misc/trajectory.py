38206f9afc1f14b5fc6f28dc375b2398b5ba3d33
Rex Han <rexhanh123@gmail.com>
2020-05-13 12:17:22 -0700

    Added trajectory class

--- data_generation/misc/trajectory.py
+++ data_generation/misc/trajectory.py
@@ -69,6 +69,7 @@ class Trajectory:
         self.h_pos = h_pos
         self.c_pos = c_pos
         self.__setupDirectory()
+
     def __setupDirectory(self):
         # Setup
         assert os.path.isdir(
@@ -83,6 +84,7 @@ class Trajectory:
             os.mkdir(self.rgb_path)
         if not os.path.isdir(self.depth_path):
             os.mkdir(self.depth_path)
+
     def get_linear_trajectory(self):
         p = create_params()
 
@@ -101,10 +103,11 @@ class Trajectory:
 
         # Specify starting state for camera and human.
         # Specified as [x (meters), y (meters), theta (radians)] coordinates
+        start_human_pos = self.h_pos
         human_pos = self.h_pos
         cam_pos = self.c_pos
-
         r.add_human_at_position_with_speed(start_human_pos, self.h_speed, identity_rng, mesh_rng)
+
         for i in range(self.num_images):
             human_pos[0] = i * self.dt * self.h_speed * np.cos(start_human_pos[2]) + start_human_pos[0]
             human_pos[1] = i * self.dt * self.h_speed * np.sin(start_human_pos[2]) + start_human_pos[1]
@@ -120,11 +123,48 @@ class Trajectory:
             cv2.imwrite(depth_file, depth_img)
 
     def get_circle_trajectory(self):
-        pass
+        p = create_params()
 
+        r = HumANavRenderer.get_renderer(p)
+        dx_cm, traversible = r.get_config()
+        # Convert the grid spacing to units of meters. Should be 5cm for the S3DIS data
+        dx_m = dx_cm / 100.
+
+        # Set the identity seed. This is used to sample a random human identity
+        # (gender, texture, body shape)
+        identity_rng = np.random.RandomState(self.identity)
+
+        # Set the Mesh seed. This is used to sample the actual mesh to be loaded
+        # which reflects the pose of the human skeleton.
+        mesh_rng = np.random.RandomState(self.pose)
+
+        # Specify starting state for camera and human.
+        # Specified as [x (meters), y (meters), theta (radians)] coordinates
+        start_human_pos = self.h_pos
+        human_pos = self.h_pos
+        cam_pos = self.c_pos
+        r.add_human_at_position_with_speed(start_human_pos, self.h_speed, identity_rng, mesh_rng)
+        circle_center = np.array([human_pos[0] - 5, human_pos[1]])
+        curr_h_angle = human_pos[2]
+        d_theta = np.pi/self.num_images
+        for i in range (self.num_images):
+            human_pos[2] = i * d_theta + start_human_pos[2]
+            human_pos[0] = np.cos(human_pos[2]) + start_human_pos[0]
+            human_pos[1] = np.sin(human_pos[2]) + start_human_pos[1]
+            r.move_human_to_position_with_speed(human_pos, self.h_speed, mesh_rng)
+            rgb_image_1mk3, depth_image_1mk1 = render_rgb_and_depth(r, cam_pos, dx_m, human_visible=True)
+            depth_img = depth_image_1mk1[0, :, :, 0]
+            rgb_img = cv2.cvtColor(rgb_image_1mk3[0].astype(np.uint8), cv2.COLOR_RGB2BGR)
+            rgb_file = os.path.join(self.rgb_path, 'img_cir{}.png'.format(i + 1))
+            depth_file = os.path.join(self.depth_path, 'img_cir{}.png'.format(i + 1))
+            cv2.imwrite(rgb_file, rgb_img)
+            cv2.imwrite(depth_file, depth_img)
+            print("Image {}".format(i))
 
 if __name__ == '__main__':
-    start_camera_pos = np.array([[7.5, 12., -1.3]])
+    start_camera_pos = np.array([[7.5, 15., -1.5]])
     start_human_pos = np.array([8.0, 9.75, np.pi/2])
-    a = Trajectory(area='area3', h_pos=start_human_pos, c_pos=start_camera_pos, h_speed=1, c_speed=0.4, identity=48, pose=20, total_time=1, num_images=2, out_dir='/home/rex/test', seq_name='test_moving')
+    a = Trajectory(area='area3', h_pos=start_human_pos, c_pos=start_camera_pos, h_speed=1, c_speed=0,
+                   identity=48, pose=20, total_time=1, num_images=10, out_dir='/home/rex/test', seq_name='test_moving')
     a.get_linear_trajectory()
+    # a.get_circle_trajectory()
