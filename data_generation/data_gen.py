cf872d2bc8d88b7d68633db43f81f606460b3f46
Shawn Shacterman <shawnshact@gmail.com>
2020-05-15 19:47:50 -0700

    added examples for generatinh multiple humans

--- data_generation/data_gen.py
+++ data_generation/data_gen.py
@@ -2,20 +2,57 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import uuid
 from dotmap import DotMap
 
 from humanav.humanav_renderer import HumANavRenderer
 from humanav.renderer_params import create_params as create_base_params
 from humanav.renderer_params import get_surreal_texture_dir
 
-from utils.visualization import render_rgb_and_depth
+from utils.visualization import render_rgb_and_depth, plot_images, plot_traversible
+class Human(object):
+    def __init__(self, speed = None, identity_seed = None, mesh_seed = None, id=None):
+        if id is None:
+            self.id = uuid.uuid4().hex # generate completely random id
+        else:
+            self.id = id
+        
+        if speed is None:
+            self.speed = 1.7*np.random.rand()
+        else:
+            self.speed = speed
+
+        if identity_seed is None:
+            self.identity_rng = np.random.RandomState(np.random.randint(100))
+        else:
+            self.identity_rng = np.random.RandomState(identity_seed)
+        
+        if mesh_seed is None:
+            self.mesh_rng = np.random.RandomState(np.random.randint(100))
+        else:
+            self.mesh = np.random.RandomState(mesh_seed)
+        
 
 class DataGenerator(object):
-    def __init__(self):
+    def __init__(self, cam_angle=None, cam_rng=None, h_rng=None):
         self.p = self.create_params()
         self._r = HumANavRenderer.get_renderer(self.p)
         self.initialize_occupancy_map_for_grid()
-        self.rng = np.random.RandomState(69)
+        
+        if cam_angle is None:
+            self.cam_angle = 2*np.pi*np.random.rand()
+        else:
+            self.cam_angle = cam_angle % (2*np.pi)
+
+        if cam_rng is None:
+            self.cam_rng = np.random.RandomState(np.random.randint(100))
+        else:
+            self.cam_rng = np.random.RandomState(cam_rng)
+        
+        if h_rng is None:
+            self.h_rng = np.random.RandomState(np.random.randint(100))
+        else:
+            self.h_rng = np.random.RandomState(h_rng)
 
     def create_params(self, area_name='area5a', width=1024, height=1024, fov_v=75., fov_h=75., cam_elevation_deg=-10):
         p = create_base_params()
@@ -44,7 +81,7 @@ class DataGenerator(object):
         associated parameters/ instance variables
         """
         resolution, traversible = self._r.get_config()
-
+        self.traversible = traversible
         self.p.dx = resolution / 100.  # To convert to metres.
 
         # Reverse the shape as indexing into the traverible is reversed ([y, x] indexing)
@@ -58,7 +95,7 @@ class DataGenerator(object):
 
         self.occupancy_grid_map = np.logical_not(traversible)*1.
     
-    def sample_point(self,rng, free_xy_map=None):
+    def sample_point(self, rng, free_xy_map=None):
         """
         Samples a real world x, y point in free space on the map.
         Optionally the user can pass in free_xy_m2 a list of m (x, y)
@@ -79,38 +116,51 @@ class DataGenerator(object):
         world_pos = (pos + self.p.map_origin)*self.p.dx
         return world_pos.astype(dtype)
 
-    def sample_free_position(self):
+    def sample_free_position(self, rng, mode='camera'):
         """
         samples a pose (x,y,theta) on free area in traversible
         """
         free_pos = np.empty(shape=(1,3))
-        x_y = self.sample_point(self.rng)
-        #theta = 2*np.pi*np.random.rand()
-        theta = np.pi
+        x_y = self.sample_point(rng)
+        if mode.lower() != 'camera' and mode.lower() != 'cam':
+            theta = 2*np.pi*np.random.rand()
+        else:
+            theta = self.cam_angle
         free_pos[0,:2] = x_y
         free_pos[0, 2] = theta
+        if mode.lower() != 'camera' and mode.lower() != 'cam':
+            return free_pos[0] 
         return free_pos
 
-    def plot_random_scene(self):
-        identity_1 = np.random.RandomState(np.random.randint(100))
-        identity_2 = np.random.RandomState(np.random.randint(100))
-        start_camera_pos = self.sample_free_position()
-        start_human_pos_1 = (start_camera_pos + np.array([[-2,0.5,-np.pi]]))[0]
-        start_human_pos_2 = (start_camera_pos + np.array([[-2,-0.5,-np.pi]]))[0]
-        h_speed = 0.7
-        self._r.add_human_at_position_with_speed(start_human_pos_1, h_speed, identity_1, self.rng)
-        self._r.add_human_at_position_with_speed(start_human_pos_2, h_speed, identity_2, self.rng)
-        self._r.remove_human()
-        rgb_image_1mk3, _ = render_rgb_and_depth(self._r, start_camera_pos, self.p.dx, human_visible=True)
-        rgb_img = rgb_image_1mk3[0].astype(np.uint8)
-        plt.imshow(rgb_img)
-        plt.show()
+    def plot_random_scene(self, num_humans=50):
+        startpos_h_lst = [self.sample_free_position(self.h_rng, mode='human') for i in range(num_humans)]
+        humans = [Human() for i in range(num_humans)]
+        for i in range(num_humans):
+            self._r.add_human_at_position_with_speed(humans[i], startpos_h_lst[i])
+
+        startpos_cam = self.sample_free_position(self.cam_rng, mode='cam') 
+        goal_pos = self.sample_free_position(self.cam_rng, mode='goal')
+        rgb_image_1mk3, depth = render_rgb_and_depth(self._r, startpos_cam, self.p.dx, human_visible=True, depth=True)
+        #rgb_img = rgb_image_1mk3[0].astype(np.uint8)
+        plot_name = 'test{}.png'.format(uuid.uuid4().hex)
+        plot_images(rgb_image_1mk3, depth, self._r.building.traversible, self.p.dx, startpos_cam, startpos_h_lst, plot_name)
+        #plot_traversible(self._r.building.map.traversible, self.p.dx, startpos_cam, startpos_h_lst, goal_pos)
 
 
-    
 if __name__ == '__main__':
-    data_gen = DataGenerator()
-    data_gen.plot_random_scene()
-    print('bitches in the club')
+    data_gen = DataGenerator(cam_angle=np.pi, cam_rng=69)
+    human1, human2, human3 = [Human() for i in range(3)]
+    startpos_cam = data_gen.sample_free_position(data_gen.cam_rng, mode='cam')
+    h_pos1 = startpos_cam[0]+[-2,-0.5,np.pi]
+    h_pos2 = startpos_cam[0]+[-3,0,np.pi]
+    h_pos3 = startpos_cam[0]+[-2,0.5,np.pi]
+    h_lst = [h_pos1, h_pos2, h_pos3]
+    data_gen._r.add_human_at_position_with_speed(human1, h_pos1)
+    data_gen._r.add_human_at_position_with_speed(human2, h_pos2)
+    data_gen._r.add_human_at_position_with_speed(human3, h_pos3)
+    rgb_image_1mk3, depth = render_rgb_and_depth(data_gen._r, startpos_cam, data_gen.p.dx, human_visible=True, depth=True)
+    plot_name = 'test{}.png'.format(uuid.uuid4().hex)
+    plot_images(rgb_image_1mk3, depth, data_gen._r.building.traversible, data_gen.p.dx, startpos_cam, h_lst, plot_name)
 
 
+# properly update traversal maps
--- data_generation/utils/visualization.py
+++ data_generation/utils/visualization.py
@@ -8,7 +8,7 @@ from humanav.renderer_params import create_params as create_base_params
 from humanav.renderer_params import get_surreal_texture_dir
 
 def plot_images(rgb_image_1mk3, depth_image_1mk1, traversible, dx_m,
-                camera_pos_13, human_pos_3, filename):
+                camera_pos_13, human_poses, filename):
 
     # Compute the real_world extent (in meters) of the traversible
     extent = [0., traversible.shape[1], 0., traversible.shape[0]]
@@ -22,11 +22,14 @@ def plot_images(rgb_image_1mk3, depth_image_1mk1, traversible, dx_m,
               vmin=-.5, vmax=1.5, origin='lower')
 
     # Plot the camera
-    ax.plot(camera_pos_13[0, 0], camera_pos_13[0, 1], 'bo', markersize=10, label='Camera')
+    ax.plot(camera_pos_13[0, 0], camera_pos_13[0, 1], 'bo', markersize=10)
     ax.quiver(camera_pos_13[0, 0], camera_pos_13[0, 1], np.cos(camera_pos_13[0, 2]), np.sin(camera_pos_13[0, 2]))
     # Plot the human
-    ax.plot(human_pos_3[0], human_pos_3[1], 'ro', markersize=10, label='Human')
-    ax.quiver(human_pos_3[0], human_pos_3[1], np.cos(human_pos_3[2]), np.sin(human_pos_3[2]))
+    i = 1
+    for human_pos in human_poses:
+        ax.plot(human_pos[0], human_pos[1], 'ro', markersize=10)
+        ax.quiver(human_pos[0], human_pos[1], np.cos(human_pos[2]), np.sin(human_pos[2]))
+        i+=1 
 
     ax.legend()
     ax.set_xlim([camera_pos_13[0, 0]-5., camera_pos_13[0, 0]+5.])
@@ -41,8 +44,9 @@ def plot_images(rgb_image_1mk3, depth_image_1mk1, traversible, dx_m,
     ax.set_xticks([])
     ax.set_yticks([])
     ax.set_title('RGB')
-
+    
     # Plot the Depth Image
+    f=667.2513908946974
     ax = fig.add_subplot(1, 3, 3)
     ax.imshow(depth_image_1mk1[0, :, :, 0].astype(np.uint8), cmap='gray')
     ax.set_xticks([])
@@ -66,3 +70,24 @@ def render_rgb_and_depth(r, camera_pos_13, dx_m, human_visible=True, rgb = True,
         depth_image_1mk1, _, _ = r._get_depth_image(camera_grid_world_pos_12, camera_pos_13[:, 2:3], xy_resolution=.05, map_size=1500, pos_3=camera_pos_13[0, :3], human_visible=True)
 
     return rgb_image_1mk3, depth_image_1mk1 
+
+
+def plot_traversible(traversible, dx_m, camera_pos, human_poses, goal_pos):
+    plt.figure(figsize=(20,10))
+    extent = [0., traversible.shape[1], 0., traversible.shape[0]]
+    extent = np.array(extent)*dx_m
+    plt.imshow(traversible, extent=extent, cmap='gray', origin='lower')
+
+    # Plot the camera
+    plt.plot(camera_pos[0, 0], camera_pos[0, 1], 'bX', markersize=5)
+    #plt.quiver(camera_pos[0, 0], camera_pos[0, 1], np.cos(camera_pos[0, 2]), np.sin(camera_pos[0, 2]), color='blue')
+    # Plot the human
+    for human_pos in human_poses:
+        plt.plot(human_pos[0], human_pos[1], 'ro', markersize=5)
+        #plt.quiver(human_pos[0], human_pos[1], np.cos(human_pos[2]), np.sin(human_pos[2]), color='red')
+
+    # Plot Goal
+    plt.plot(goal_pos[0], goal_pos[1], 'gX', markersize=5)
+    plt.title('Topview')
+    plt.show()
+
